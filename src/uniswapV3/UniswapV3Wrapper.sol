4d7a0a1d2a1a8a5016ea05b2b0b65f333ade0e0e
ultrasecr.eth <241804+ultrasecreth@users.noreply.github.com>
2023-08-01 21:26:07 +0100

    Tune up Uniswap wrapper

--- src/uniswapV3/UniswapV3Wrapper.sol
+++ src/uniswapV3/UniswapV3Wrapper.sol
@@ -4,41 +4,73 @@ pragma solidity ^0.8.0;
 
 import { IUniswapV3FlashCallback } from "./interfaces/callback/IUniswapV3FlashCallback.sol";
 import { IUniswapV3Pool } from "./interfaces/IUniswapV3Pool.sol";
-import { IUniswapV3Factory } from "./interfaces/IUniswapV3Factory.sol";
+import { PoolAddress } from "./interfaces/PoolAddress.sol";
 
 import { IERC20 } from "lib/erc3156pp/src/interfaces/IERC20.sol";
 
 import { BaseWrapper } from "../BaseWrapper.sol";
 
 contract UniswapV3Wrapper is BaseWrapper, IUniswapV3FlashCallback {
+    using PoolAddress for address;
+
     // CONSTANTS
-    IUniswapV3Factory public immutable factory;
+    address public immutable factory;
 
     // ACCESS CONTROL
     IUniswapV3Pool internal _activePool;
 
     // DEFAULT ASSETS
     IERC20 weth;
-    IERC20 dai;
+    IERC20 usdc;
+    IERC20 usdt;
 
     /// @param factory_ Uniswap v3 UniswapV3Factory address
     /// @param weth_ Weth contract used in Uniswap v3 Pairs
-    /// @param dai_ dai contract used in Uniswap v3 Pairs
-    constructor(IUniswapV3Factory factory_, IERC20 weth_, IERC20 dai_) {
+    /// @param usdc_ usdc contract used in Uniswap v3 Pairs
+    /// @param usdt_ usdt contract used in Uniswap v3 Pairs
+    constructor(address factory_, IERC20 weth_, IERC20 usdc_, IERC20 usdt_) {
         factory = factory_;
         weth = weth_;
-        dai = dai_;
+        usdc = usdc_;
+        usdt = usdt_;
     }
 
     /**
      * @dev Get the Uniswap Pool that will be used as the source of a loan. The opposite asset will be Weth, except for
-     * Weth that will be Dai.
+     * Weth that will be usdc.
      * @param asset The loan currency.
-     * @return The Uniswap V3 Pool that will be used as the source of the flash loan.
+     * @param amount The amount of assets to borrow.
+     * @return pool The Uniswap V3 Pool that will be used as the source of the flash loan.
      */
-    function getPool(IERC20 asset) public view returns (IUniswapV3Pool) {
-        IERC20 assetOther = asset == weth ? dai : weth;
-        return IUniswapV3Pool(factory.getPool(address(asset), address(assetOther), 3000));
+    function getPool(IERC20 asset, uint256 amount) public view returns (IUniswapV3Pool pool) {
+        // Try a stable pair first
+        pool = _checkPool(asset, asset == usdc ? usdt : usdc, 0.0001e6, amount);
+        if (address(pool) != address(0)) return pool;
+
+        // Look for the cheapest fee otherwise
+        uint16[3] memory fees = [0.0005e6, 0.003e6, 0.01e6];
+        IERC20 assetOther = asset == weth ? usdc : weth;
+        for (uint256 i = 0; i < 3; i++) {
+            pool = _checkPool(asset, assetOther, fees[i], amount);
+            if (address(pool) != address(0)) return pool;
+        }
+
+        pool = IUniswapV3Pool(address(0));
+    }
+
+    function _checkPool(
+        IERC20 asset,
+        IERC20 other,
+        uint24 fee,
+        uint256 amount
+    )
+        internal
+        view
+        returns (IUniswapV3Pool poolAddress)
+    {
+        PoolAddress.PoolKey memory poolKey = PoolAddress.getPoolKey(address(asset), address(other), fee);
+        poolAddress = IUniswapV3Pool(factory.computeAddress(poolKey));
+        poolAddress = asset.balanceOf(address(poolAddress)) >= amount ? poolAddress : IUniswapV3Pool(address(0));
     }
 
     /**
@@ -48,13 +80,13 @@ contract UniswapV3Wrapper is BaseWrapper, IUniswapV3FlashCallback {
      * @return The amount of `asset` to be charged for the loan, on top of the returned principal.
      */
     function flashFee(IERC20 asset, uint256 amount) public view override returns (uint256) {
-        IUniswapV3Pool pool = getPool(asset);
-        if (asset.balanceOf(address(pool)) < amount) return type(uint256).max;
+        IUniswapV3Pool pool = getPool(asset, amount);
+        if (address(pool) == address(0)) return type(uint256).max;
         return amount * uint256(pool.fee()) / 1e6;
     }
 
     function _flashLoan(IERC20 asset, uint256 amount, bytes memory data) internal override {
-        IUniswapV3Pool pool = getPool(asset);
+        IUniswapV3Pool pool = getPool(asset, amount);
         require(address(pool) != address(0), "Unsupported currency");
 
         IERC20 asset0 = IERC20(pool.token0());
--- test/UniswapV3Wrapper.t.sol
+++ test/UniswapV3Wrapper.t.sol
@@ -14,8 +14,10 @@ import { IUniswapV3Factory } from "../src/uniswapV3/interfaces/IUniswapV3Factory
 contract UniswapV3WrapperTest is PRBTest, StdCheats {
     UniswapV3Wrapper internal wrapper;
     FlashBorrower internal borrower;
-    IERC20 internal dai;
+    IERC20 internal usdc;
+    IERC20 internal usdt;
     IERC20 internal weth;
+    IERC20 internal wbtc;
     IUniswapV3Factory internal factory;
 
     /// @dev A function invoked before each test case is run.
@@ -28,27 +30,33 @@ contract UniswapV3WrapperTest is PRBTest, StdCheats {
 
         vm.createSelectFork({ urlOrAlias: "mainnet", blockNumber: 17_784_898 });
         factory = IUniswapV3Factory(0x1F98431c8aD98523631AE4a59f267346ea31F984);
-        dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+        usdc = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+        usdt = IERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7);
         weth = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+        wbtc = IERC20(0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599);
 
-        wrapper = new UniswapV3Wrapper(factory, weth, dai);
+        wrapper = new UniswapV3Wrapper(address(factory), weth, usdc, usdt);
         borrower = new FlashBorrower(wrapper);
-        deal(address(dai), address(this), 1e18); // For fees
+        deal(address(usdc), address(this), 1e6); // For fees
     }
 
     /// @dev Basic test. Run it with `forge test -vvv` to see the console log.
     function test_flashFee() external {
         console2.log("test_flashFee");
-        assertEq(wrapper.flashFee(dai, 1e18), 0.003e18, "Fee not exact");
-        assertEq(wrapper.flashFee(dai, type(uint256).max), type(uint256).max, "Fee not max");
+        assertEq(wrapper.flashFee(usdc, 100e6), 0.01e6, "Fee not exact USDC");
+        assertEq(wrapper.flashFee(usdt, 100e6), 0.01e6, "Fee not exact USDT");
+        assertEq(wrapper.flashFee(weth, 1e18), 0.0001e18, "Fee not exact WETH 1");
+        assertEq(wrapper.flashFee(weth, 10e18), 0.005e18, "Fee not exact WETH 2");
+        assertEq(wrapper.flashFee(wbtc, 1e8), 0.0005e8, "Fee not exact WBTC");
+        assertEq(wrapper.flashFee(usdc, type(uint256).max), type(uint256).max, "Fee not max");
     }
 
     function test_flashLoan() external {
         console2.log("test_flashLoan");
-        uint256 loan = 1e18;
-        uint256 fee = wrapper.flashFee(dai, loan);
-        dai.transfer(address(borrower), fee);
-        bytes memory result = borrower.flashBorrow(dai, loan);
+        uint256 loan = 100e6;
+        uint256 fee = wrapper.flashFee(usdc, loan);
+        usdc.transfer(address(borrower), fee);
+        bytes memory result = borrower.flashBorrow(usdc, loan);
 
         // Test the return values
         (bytes32 callbackReturn) = abi.decode(result, (bytes32));
@@ -56,7 +64,7 @@ contract UniswapV3WrapperTest is PRBTest, StdCheats {
 
         // Test the borrower state
         assertEq(borrower.flashInitiator(), address(borrower), "flashInitiator");
-        assertEq(address(borrower.flashAsset()), address(dai), "flashAsset");
+        assertEq(address(borrower.flashAsset()), address(usdc), "flashAsset");
         assertEq(borrower.flashAmount(), loan, "flashAmount");
         // The amount we transferred to pay for fees, plus the amount we borrowed
         assertEq(borrower.flashBalance(), loan + fee, "flashBalance");
