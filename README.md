11a11f6d861d375e34f40beb275b22f1e06649ae
alcueca <alberto@yield.is>
2023-10-18 11:32:38 +0100

    Deployed on L2s

--- README.md
+++ README.md
@@ -28,11 +28,11 @@ Contracts are deployed at the same address for all supported networks.
 
 | Contract | Lender | Address | Networks |
 | ---- | ---- | ---- | ---- |
-|[AaveWrapper](src/aave/AaveWrapper.sol)|Aave v3|0x02C7632b84B3447845531541d0285D67E656e50c|Arbitrum One, Optimism|
-|[BalancerWrapper](src/balancer/BalancerWrapper.sol)|Balancer v2|0x3d4DF8596e5750A4F721c8764d585dcc8623d009|Arbitrum One, Optimism|
-|[UniswapV3Wrapper](src/uniswapV3/UniswapV3Wrapper.sol)|Uniswap v3|0x23de8e0bB91A105bEFf9d40d8d75C1A9fE40f523|Arbitrum One, Optimism|
+|[AaveWrapper](src/aave/AaveWrapper.sol)|Aave v3|0x61dD1523916471dc01c947d91EF91dfb8E9f7C08|Arbitrum One, Optimism, Polygon|
+|[BalancerWrapper](src/balancer/BalancerWrapper.sol)|Balancer v2|0x8630CE1b6AC9a99C6A0fC776b559582C8f1b9E93|Arbitrum One, Optimism, Polygon|
+|[UniswapV3Wrapper](src/uniswapV3/UniswapV3Wrapper.sol)|Uniswap v3|0x24030f0B35b602778a4CD4D7b8Da3088c4bAC461|Arbitrum One, Optimism, Polygon|
 
-When a contract requires constructor parameters which vary per network, these are supplied by the [Registry](https://github.com/alcueca/registry) deployed at 0x05caE14d1A348B29d2b169697b4BE51009a9C4dF in each supported network.
+When a contract requires constructor parameters which vary per network, these are supplied by the [Registry](https://github.com/alcueca/registry) deployed at 0x1BFf8Eee6ECF1c8155E81dba8894CE9cF49a220c in each supported network.
 
 ## Flash Loans
 
---
+++ script/AaveDeploy.s.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.19 <=0.9.0;
+
+import { Script } from "forge-std/Script.sol";
+
+import { console2 } from "forge-std/console2.sol";
+
+import { AaveWrapper, IPoolAddressesProvider } from "../src/aave/AaveWrapper.sol";
+
+/// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
+contract AaveDeploy is Script {
+    enum Network {
+        MAINNET,
+        ARBITRUM,
+        POLYGON,
+        OPTIMISM
+    }
+    bytes32 public constant SALT = keccak256("alcueca-1");
+    Network public constant NETWORK = Network.POLYGON;
+
+    mapping(Network network => IPoolAddressesProvider) public providers;
+
+    address internal factory = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
+
+    constructor () {
+        providers[Network.MAINNET] = IPoolAddressesProvider(0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e);
+        providers[Network.ARBITRUM] = IPoolAddressesProvider(0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb);
+        providers[Network.POLYGON] = IPoolAddressesProvider(0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb);
+        providers[Network.OPTIMISM] = IPoolAddressesProvider(0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb);
+    }
+
+
+    IPoolAddressesProvider internal provider = IPoolAddressesProvider(0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e);
+
+
+    function run() public {
+        console2.log("Deploying as %s", msg.sender);
+
+        vm.startBroadcast();
+
+        console2.log("Aave: %s", address(providers[NETWORK]));
+        AaveWrapper aaveWrapper = new AaveWrapper{salt: SALT}(providers[NETWORK]);
+        console2.log("AaveWrapper deployed at: %s", address(aaveWrapper));
+
+        vm.stopBroadcast();
+    }
+}
