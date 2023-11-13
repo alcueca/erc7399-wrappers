a73992ce49a9b719ac1be5877fbee84b869d93e3
Alberto Cuesta Cañada <38806121+alcueca@users.noreply.github.com>
2023-11-13 14:12:21 +0000

    Alternative flash loan entry point without function pointers (#16)
    * feat: Provide an alternative flash loan entry point without function pointers
    
    * forge fmt
    
    * Fixed lint
    
    ---------
    
    Co-authored-by: alcueca <alberto@yield.is>

---
+++ src/utils/FunctionCodec.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+library FunctionCodec {
+    function encodeParams(address contractAddr, bytes4 selector) internal pure returns (bytes24) {
+        return bytes24(bytes20(contractAddr)) | bytes24(selector) >> 160;
+    }
+
+    function decodeParams(bytes24 encoded) internal pure returns (address contractAddr, bytes4 selector) {
+        contractAddr = address(bytes20(encoded));
+        selector = bytes4(encoded << 160);
+    }
+
+    function encodeFunction(
+        function(address, address, address, uint256, uint256, bytes memory) external returns (bytes memory) f
+    )
+        internal
+        pure
+        returns (bytes24)
+    {
+        return encodeParams(f.address, f.selector);
+    }
+
+    function decodeFunction(
+        address contractAddr,
+        bytes4 selector
+    )
+        internal
+        pure
+        returns (function(address, address, address, uint256, uint256, bytes memory) external returns (bytes memory) f)
+    {
+        uint32 s = uint32(selector);
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            f.address := contractAddr
+            f.selector := s
+        }
+    }
+
+    function decodeFunction(bytes24 encoded)
+        internal
+        pure
+        returns (function(address, address, address, uint256, uint256, bytes memory) external returns (bytes memory) f)
+    {
+        (address contractAddr, bytes4 selector) = decodeParams(encoded);
+        return decodeFunction(contractAddr, selector);
+    }
+}
