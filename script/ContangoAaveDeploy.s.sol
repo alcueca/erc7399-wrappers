c8b04f09ea8a31f9ff1979ce3eecbfe7975d2661
ultrasecr.eth <241804+ultrasecreth@users.noreply.github.com>
2024-09-28 20:56:08 +0100

    Deploy ContangoWrappers for new Aave deployments

--- script/ContangoAaveDeploy.s.sol
+++ script/ContangoAaveDeploy.s.sol
@@ -18,62 +18,109 @@ contract ContangoAaveDeploy is Script {
         address addressProvider;
     }
 
-    bytes32 public constant SALT = keccak256("ultrasecr.eth-2");
+    bytes32 public constant SALT = keccak256("ultrasecr.eth");
 
     Registry internal registry = Registry(0xa348320114210b8F4eaF1b0795aa8F70803a93EA);
 
     address public constant TIMELOCK = 0xc0939a4Ed0129bc5162F6f693935B3F72a46a90D;
     address public constant CONTANGO = 0x6Cae28b3D09D8f8Fc74ccD496AC986FC84C0C24E;
 
-    mapping(Network network => IPoolAddressesProviderV3 addressProvider) public deployParams;
+    struct DeployParams {
+        string name;
+        IPoolAddressesProviderV3 addressProvider;
+    }
+
+    mapping(Network network => DeployParams[] params) public deployParams;
 
     constructor() {
-        deployParams[MAINNET] = IPoolAddressesProviderV3(0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e);
-        deployParams[ARBITRUM] = IPoolAddressesProviderV3(0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb);
-        deployParams[POLYGON] = IPoolAddressesProviderV3(0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb);
-        deployParams[OPTIMISM] = IPoolAddressesProviderV3(0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb);
-        deployParams[BASE] = IPoolAddressesProviderV3(0xe20fCBdBfFC4Dd138cE8b2E6FBb6CB49777ad64D);
-        deployParams[GNOSIS] = IPoolAddressesProviderV3(0x36616cf17557639614c1cdDb356b1B83fc0B2132);
-        deployParams[BSC] = IPoolAddressesProviderV3(0xff75B6da14FfbbfD355Daf7a2731456b3562Ba6D);
-        deployParams[AVALANCHE] = IPoolAddressesProviderV3(0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb);
-        deployParams[SCROLL] = IPoolAddressesProviderV3(0x69850D0B276776781C063771b161bd8894BCdD04);
+        deployParams[MAINNET].push(
+            DeployParams("AaveV3", IPoolAddressesProviderV3(0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e))
+        );
+        deployParams[MAINNET].push(
+            DeployParams("AaveV3Lido", IPoolAddressesProviderV3(0xcfBf336fe147D643B9Cb705648500e101504B16d))
+        );
+        deployParams[MAINNET].push(
+            DeployParams("AaveV3EtherFi", IPoolAddressesProviderV3(0xeBa440B438Ad808101d1c451C1C5322c90BEFCdA))
+        );
+        deployParams[ARBITRUM].push(
+            DeployParams("AaveV3", IPoolAddressesProviderV3(0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb))
+        );
+        deployParams[POLYGON].push(
+            DeployParams("AaveV3", IPoolAddressesProviderV3(0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb))
+        );
+        deployParams[OPTIMISM].push(
+            DeployParams("AaveV3", IPoolAddressesProviderV3(0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb))
+        );
+        deployParams[BASE].push(
+            DeployParams("AaveV3", IPoolAddressesProviderV3(0xe20fCBdBfFC4Dd138cE8b2E6FBb6CB49777ad64D))
+        );
+        deployParams[GNOSIS].push(
+            DeployParams("AaveV3", IPoolAddressesProviderV3(0x36616cf17557639614c1cdDb356b1B83fc0B2132))
+        );
+        deployParams[BSC].push(
+            DeployParams("AaveV3", IPoolAddressesProviderV3(0xff75B6da14FfbbfD355Daf7a2731456b3562Ba6D))
+        );
+        deployParams[AVALANCHE].push(
+            DeployParams("AaveV3", IPoolAddressesProviderV3(0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb))
+        );
+        deployParams[SCROLL].push(
+            DeployParams("AaveV3", IPoolAddressesProviderV3(0x69850D0B276776781C063771b161bd8894BCdD04))
+        );
     }
 
     function run() public {
         console2.log("Deploying as %s", msg.sender);
 
         Network network = currentNetwork();
-        IPoolAddressesProviderV3 addressProvider = deployParams[network];
 
-        require(address(addressProvider) != address(0), "No deploy params for this network");
+        DeployParams[] memory allDeployParams = deployParams[network];
+        for (uint256 i = 0; i < allDeployParams.length; i++) {
+            DeployParams memory _deployParams = allDeployParams[i];
+            IPoolAddressesProviderV3 addressProvider = _deployParams.addressProvider;
 
-        bool isV2 = false;
-        address poolDataProvider = address(addressProvider.getPoolDataProvider());
-        address pool = addressProvider.getPool();
+            require(address(addressProvider) != address(0), "No deploy params for this network");
 
-        bytes memory paramsBytes = abi.encode(pool, addressProvider, poolDataProvider, isV2);
-        string memory key = "AaveV3Wrapper";
-        console2.log(key);
+            bool isV2 = false;
+            address poolDataProvider = address(addressProvider.getPoolDataProvider());
+            address pool = addressProvider.getPool();
 
-        if (address(registry).code.length == 0) {
-            console2.log("Registry not deployed");
-            RegistryDeploy deploy = new RegistryDeploy();
-            deploy.run();
-        }
+            bytes memory paramsBytes = abi.encode(pool, addressProvider, poolDataProvider, isV2);
+            string memory key = string.concat(_deployParams.name, "Wrapper");
+            console2.log(key);
 
-        if (keccak256(registry.get(key)) != keccak256(paramsBytes)) {
-            console2.log("Updating registry");
+            if (address(registry).code.length == 0) {
+                console2.log("Registry not deployed");
+                RegistryDeploy deploy = new RegistryDeploy();
+                deploy.run();
+            }
+
+            if (keccak256(registry.get(key)) != keccak256(paramsBytes)) {
+                console2.log("Updating registry");
+                vm.broadcast();
+                registry.set(key, paramsBytes);
+            }
+
+            address computed = computeCreate2Address(
+                SALT,
+                hashInitCode(
+                    type(PermissionedAaveWrapper).creationCode,
+                    abi.encode(TIMELOCK, CONTANGO, registry, _deployParams.name)
+                )
+            );
+
+            if (computed.code.length > 0) {
+                console2.log("%s already deployed at: %s", _deployParams.name, computed);
+                continue;
+            }
+
             vm.broadcast();
-            registry.set(key, paramsBytes);
+            PermissionedAaveWrapper wrapper =
+                new PermissionedAaveWrapper{ salt: SALT }(TIMELOCK, CONTANGO, registry, _deployParams.name);
+            console2.log("%s deployed at: %s", key, address(wrapper));
+
+            require(wrapper.ADDRESSES_PROVIDER() == address(addressProvider), "AddressesProvider not set");
+            require(wrapper.POOL() == pool, "Pool not set");
+            require(address(wrapper.dataProvider()) == poolDataProvider, "PoolDataProvider not set");
         }
-
-        vm.broadcast();
-        PermissionedAaveWrapper wrapper =
-            new PermissionedAaveWrapper{ salt: SALT }(msg.sender, CONTANGO, registry, "AaveV3");
-        console2.log("%s deployed at: %s", key, address(wrapper));
-
-        require(wrapper.ADDRESSES_PROVIDER() == address(addressProvider), "AddressesProvider not set");
-        require(wrapper.POOL() == pool, "Pool not set");
-        require(address(wrapper.dataProvider()) == poolDataProvider, "PoolDataProvider not set");
     }
 }
